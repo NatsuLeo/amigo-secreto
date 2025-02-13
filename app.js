1d66a1a80dfeb1ef0c82e0f346705ad7ff038ac8
Leonardo Diaz <diazbecerrajonathan@hotmail.com>
2025-02-13 12:38:30 -0600

    Actualiza final index y js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function agregarAmigos(){
+    let amigos = document.getElementById("amigo").value;
+    if (amigos == ''){
+        alert('Agrega un nombre valido')
+    } else {
+        listaAmigos.push(amigos);
+        mostrarLista();
+        limpiarCaja();
+    }
+}
+
+function mostrarLista(){
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; 
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = listaAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    let sorteo = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSorteo = listaAmigos[sorteo];
+    let mostrarSorteo = document.getElementById('resultado');
+    mostrarSorteo.innerHTML = `<li>${amigoSorteo}</li>`;
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
